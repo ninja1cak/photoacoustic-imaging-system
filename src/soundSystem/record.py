947b198c50f47ed23091720ce0175ab31a2e1dd9
Hauzan Khoirullah Miftah <hauzan.khoirullah1022@students.unila.ac.id>
2025-01-18 01:21:36 +0700

    feat(audio): add function to geat peak signal around frequency laser

--- src/soundSystem/record.py
+++ src/soundSystem/record.py
@@ -31,3 +31,20 @@ def transformSignalAudioToFrequency(signalAudio, frequency=44100):
     return frequencies[:halfIndex], signalAudioInFrequencyAbsolute[:halfIndex]
 
 
+def getPeakSignalAroundFreqLaser(frequencyLaser, signalAudioInFrequency):
+    BOUND_FREQUENCY = 500
+    lowerBoundFrequency = frequencyLaser - BOUND_FREQUENCY
+    upperBoundFrequency = frequencyLaser + BOUND_FREQUENCY
+
+    indexLowerBoundFrequency = lowerBoundFrequency * 2
+    indexUpperBoundFrequency = upperBoundFrequency * 2
+
+    peakSignal, indexPeakSignal = _getMaxValueWithIndex(signalAudioInFrequency[indexLowerBoundFrequency:indexUpperBoundFrequency])
+    frequencyPeakSignal = (indexPeakSignal/2) + lowerBoundFrequency
+    return frequencyPeakSignal, peakSignal
+
+def _getMaxValueWithIndex(signal):
+    value = numpy.max(signal)
+    index = numpy.argmax(signal)
+
+    return value, index
